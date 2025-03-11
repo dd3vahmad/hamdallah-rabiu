c83450a5376b4d5d37e90462183a4b2167910c11
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-03-11 11:26:47 +0100

    Fix: Services padding

--- components/services.tsx
+++ components/services.tsx
@@ -4,10 +4,15 @@ import { services } from "@/lib/data";
 
 const Services = () => {
   return (
-    <div className="md:w-fit mx-auto grid md:grid-cols-3 gap-5 p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {services.map((service, i) => (
-        <Service key={i} service={service} />
-      ))}
+    <div className="w-full">
+      <h2 className="text-3xl font-semibold w-full text-center mb-5">
+        My Services
+      </h2>
+      <div className="md:w-fit mx-auto grid md:grid-cols-3 gap-5 px-20 font-[family-name:var(--font-geist-sans)]">
+        {services.map((service, i) => (
+          <Service key={i} service={service} />
+        ))}
+      </div>
     </div>
   );
 };
