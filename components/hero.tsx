7d71c4632c81aa1920f90d6d4b09ca2fc671febb
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-03-12 00:21:02 +0100

    Fix: Hero section responsiveness.

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,29 +4,28 @@ const Hero = () => {
   return (
     <main
       id="#home"
-      className="grid grid-cols-[2fr_1fr] min-h-[70vh] md:w-[60vw] mx-auto justify-items-center mt-25 font-[family-name:var(--font-geist-sans)]"
+      className="flex justify-center items-center md:grid grid-cols-[2fr_1fr] min-h-[70vh] md:w-[60vw] mx-auto md:justify-items-center mt-25 font-[family-name:var(--font-geist-sans)]"
     >
-      <div className="flex flex-col gap-8 row-start-2 h-full sm:p-20 sm:items-start">
-        <p className="px-4 py-2 mb-3 rounded-full font-semibold text-sm border-2 border-purple-700 -rotate-10">
+      <div className="flex flex-col gap-8 row-start-2 h-full">
+        <p className="px-4 py-2 md:mb-3 w-fit rounded-full font-semibold text-sm border-2 border-purple-700 -rotate-10">
           Welcome!
         </p>
-        <h3 className="text-3xl font-semibold">
+        <h3 className="text-xl md:text-3xl font-semibold">
           Oh, you found me? How lovely!
         </h3>
-        <h3 className="text-2xl font-semibold">Let me introduce myself</h3>
-        <h2 className="text-5xl font-bold">
+        <h3 className="text-xl md:text-2xl font-semibold">
+          Let me introduce myself
+        </h3>
+        <h2 className="text-3xl md:text-5xl font-bold">
           I am <span className="text-purple-800">Hamdallah Rabiu</span>
         </h2>
 
         <p className="text-sm font-semibold text-center sm:text-left">
-          Direct-Response{" "}
-          <span className="text-purple-800">
-            Copywriter - Nice to meet you! 😊
-          </span>
+          Nice to meet you! 😊
         </p>
       </div>
 
-      <div className="flex flex-col gap-8 row-start-2 h-full sm:p-20 sm:items-start"></div>
+      <div className="hidden md:blockflex flex-col gap-8 row-start-2 h-full sm:items-start"></div>
     </main>
   );
 };
