00f928ea6b05c7a3da7f7045f0cf096c3e0b9adf
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-03-11 18:03:56 +0100

    Add: Animation template for components.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/theme";
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
--- components/services.tsx
+++ components/services.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import Service from "./service";
 import { services } from "@/lib/data";
 import { GlassShadow } from "./ui/ui";
+import Animate from "./providers/animate";
 
 const Services = () => {
   return (
-    <div id="services" className="w-full">
-      <h2 className="text-3xl font-semibold w-full text-center my-5">
-        My Services
-      </h2>
-      <div className="md:w-fit mx-auto grid md:grid-cols-3 gap-5 px-20 font-[family-name:var(--font-geist-sans)] relative">
-        <GlassShadow />
-        {services.map((service, i) => (
-          <Service key={i} service={service} />
-        ))}
+    <Animate animationType="slideUp">
+      <div id="services" className="w-full pt-30">
+        <h2 className="text-3xl font-semibold w-full text-center my-5">
+          My <span className="text-primary">Services</span>
+        </h2>
+        <div className="md:w-fit mx-auto grid md:grid-cols-3 gap-5 px-20 font-[family-name:var(--font-geist-sans)] relative">
+          <GlassShadow />
+          {services.map((service, i) => (
+            <Service key={i} service={service} />
+          ))}
+        </div>
       </div>
-    </div>
+    </Animate>
   );
 };
 
