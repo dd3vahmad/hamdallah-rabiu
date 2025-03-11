8ff3c430036e57d638e1ffa4281635b21b421c30
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-03-11 10:01:18 +0100

    Add: Services images.

--- assets/index.ts
+++ assets/index.ts
@@ -1,7 +1,23 @@
 import logoDark from "./logo-dark.png";
 import logoLight from "./logo-light.png";
+import branding from "./branding.png";
+import consulting from "./consulting.png";
+import copywriting from "./copywriting.png";
+import marketing from "./marketing.png";
+import mastermind from "./mastermind.png";
+import messaging from "./messaging.png";
 
 const isDark = false;
 
 const logo = isDark ? logoDark : logoLight;
-export { logoDark, logoLight, logo };
+export {
+  logoDark,
+  logoLight,
+  logo,
+  branding,
+  consulting,
+  copywriting,
+  marketing,
+  mastermind,
+  messaging,
+};
